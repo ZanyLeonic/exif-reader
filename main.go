c6d67c0f6e8e632d33b9d8d501c96cc3f062a83e
Leo Durrant <leodurrant@microsoft.com>
2025-12-22 18:28:23 +0000

    wip: got make and model tags

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -42,9 +44,66 @@ func extractExifData(data []byte) (*PhotoExifEvidence, error) {
 		return nil, errors.New("file is not a JPEG")
 	}
 
+	var endian binary.ByteOrder
 	for i := range data {
 		if data[i] == 0xFF && data[i+1] == 0xE1 {
 			slog.Info("Found APP1 segment")
+
+			if data[i+10] == 0x49 && data[i+11] == 0x49 {
+				endian = binary.LittleEndian
+			} else if data[i+10] == 0x4D && data[i+11] == 0x4D {
+				endian = binary.BigEndian
+			} else {
+				return nil, errors.New("unsupported byte order")
+			}
+			slog.Info("detected photo endianess from TIFF header", "endian", endian)
+
+			tiffStart := i + 10
+			ifdOffset := endian.Uint32(data[tiffStart+4 : tiffStart+8])
+			firstIfdIndex := tiffStart + int(ifdOffset)
+
+			slog.Info("First IFD Index", "first", firstIfdIndex)
+
+			entryCount := endian.Uint16(data[firstIfdIndex : firstIfdIndex+2])
+			slog.Info("IFD entry count", "count", entryCount)
+
+			for j := 0; j < int(entryCount); j++ {
+				entryOffset := firstIfdIndex + 2 + (j * 12)
+
+				tag := endian.Uint16(data[entryOffset : entryOffset+2])
+				dataType := endian.Uint16(data[entryOffset+2 : entryOffset+4])
+				count := endian.Uint32(data[entryOffset+4 : entryOffset+8])
+				valueOffset := endian.Uint32(data[entryOffset+8 : entryOffset+12])
+
+				slog.Info("IFD Entry",
+					"tag", tag,
+					"type", dataType,
+					"count", count,
+					"valueOffset", valueOffset)
+
+				if tag == 0x010F {
+					cameraMake := ""
+					if count <= 4 {
+						cameraMake = string(data[entryOffset+8 : entryOffset+8+int(count)])
+					} else {
+						offset := tiffStart + int(valueOffset)
+						cameraMake = string(data[offset : offset+int(count)])
+					}
+
+					slog.Info("Found Make tag", "make", strings.TrimRight(cameraMake, "\x00"))
+				} else if tag == 0x0110 {
+					cameraModel := ""
+					if count <= 4 {
+						cameraModel = string(data[entryOffset+8 : entryOffset+8+int(count)])
+					} else {
+						offset := tiffStart + int(valueOffset)
+						cameraModel = string(data[offset : offset+int(count)])
+					}
+
+					slog.Info("Found Model tag", "model", strings.TrimRight(cameraModel, "\x00"))
+				}
+			}
+
 			return &PhotoExifEvidence{}, nil
 		}
 	}
