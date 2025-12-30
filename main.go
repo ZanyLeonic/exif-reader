4c02f0a32d7d69422312152a0a23d703380631af
Leo Durrant <leodurrant@microsoft.com>
2025-12-30 18:40:27 +0000

    wip: added additional tags for context

--- main.go
+++ main.go
@@ -14,41 +14,219 @@ type EXIFTag uint16
 
 // APP1 IFD Tags
 const (
-	Make        EXIFTag = 0x010f
-	Model       EXIFTag = 0x0110
-	Orientation EXIFTag = 0x0112
-	Software    EXIFTag = 0x0131
-	EXIFSubIFD  EXIFTag = 0x8769
-	GPSSubIFD   EXIFTag = 0x8825
+	ProcessingSoftware EXIFTag = 0x000b
+	ImageWidth         EXIFTag = 0x0100
+	ImageHeight        EXIFTag = 0x0101
+	ImageDescription   EXIFTag = 0x010e
+	Make               EXIFTag = 0x010f
+	Model              EXIFTag = 0x0110
+	Orientation        EXIFTag = 0x0112
+	Software           EXIFTag = 0x0131
+	ModifyDate         EXIFTag = 0x0132
+	Artist             EXIFTag = 0x013b
+	EXIFSubIFD         EXIFTag = 0x8769
+	GPSSubIFD          EXIFTag = 0x8825
+	XPTitle            EXIFTag = 0x9c9b
+	XPComment          EXIFTag = 0x9c9c
+	XPAuthor           EXIFTag = 0x9c9d
+	XPKeywords         EXIFTag = 0x9c9e
+	XPSubject          EXIFTag = 0x9c9f
 )
 
 // EXIF Sub-IFD Tags
 const (
-	DateCaptured    EXIFTag = 0x9003
-	PixelXDimension EXIFTag = 0xa002
-	PixelYDimension EXIFTag = 0xa003
-	FlashFired      EXIFTag = 0x9209
+	Copyright               EXIFTag = 0x8298
+	ExposureTime            EXIFTag = 0x829a
+	FNumber                 EXIFTag = 0x829d
+	ExposureProgram         EXIFTag = 0x8822
+	ISO                     EXIFTag = 0x8827
+	ExifVersion             EXIFTag = 0x9000
+	DateCaptured            EXIFTag = 0x9003
+	CreateDate              EXIFTag = 0x9004
+	OffsetTime              EXIFTag = 0x9010
+	OffsetTimeOriginal      EXIFTag = 0x9011
+	OffsetTimeDigitized     EXIFTag = 0x9012
+	ComponentsConfiguration EXIFTag = 0x9101
+	MeteringMode            EXIFTag = 0x9207
+	LightSource             EXIFTag = 0x9208
+	FlashFired              EXIFTag = 0x9209
+	FocalLength             EXIFTag = 0x920a
+	MakerNote               EXIFTag = 0x927c
+	UserComment             EXIFTag = 0x9286
+	SubSecTime              EXIFTag = 0x9290
+	SubSecTimeOriginal      EXIFTag = 0x9291
+	SubSecTimeDigitized     EXIFTag = 0x9292
+	FlashpixVersion         EXIFTag = 0xa000
+	ColorSpace              EXIFTag = 0xa001
+	PixelXDimension         EXIFTag = 0xa002
+	PixelYDimension         EXIFTag = 0xa003
+	RelatedSoundFile        EXIFTag = 0xa004
+	FileSource              EXIFTag = 0xa300
+	SceneType               EXIFTag = 0xa301
+	WhiteBalance            EXIFTag = 0xa403
+	DigitalZoomRatio        EXIFTag = 0xa404
+	SceneCaptureType        EXIFTag = 0xa406
+	Contrast                EXIFTag = 0xa408
+	Saturation              EXIFTag = 0xa409
+	Sharpness               EXIFTag = 0xa40a
+	SubjectDistanceRange    EXIFTag = 0xa40c
+	ImageUniqueID           EXIFTag = 0xa420
+	BodySerialNumber        EXIFTag = 0xa431
+	LensInfo                EXIFTag = 0xa432
+	LensMake                EXIFTag = 0xa433
+	LensModel               EXIFTag = 0xa434
+	LensSerialNumber        EXIFTag = 0xa435
+	ImageEditor             EXIFTag = 0xa438
+	CameraFirmware          EXIFTag = 0xa439
+	CompositeImage          EXIFTag = 0xa460
+	CompositeImageCount     EXIFTag = 0xa461
+	SerialNumber            EXIFTag = 0xfde9
 )
 
 // GPS Sub-IFD Tags
 const (
-	LatitudeDir  EXIFTag = 0x1
-	Latitude     EXIFTag = 0x2
-	LongitudeDir EXIFTag = 0x3
-	Longitude    EXIFTag = 0x4
+	GPSVersionID     EXIFTag = 0x0
+	LatitudeRef      EXIFTag = 0x1
+	Latitude         EXIFTag = 0x2
+	LongitudeRef     EXIFTag = 0x3
+	Longitude        EXIFTag = 0x4
+	AltitudeRef      EXIFTag = 0x5
+	Altitude         EXIFTag = 0x6
+	Timestamp        EXIFTag = 0x7
+	SpeedRef         EXIFTag = 0x0c
+	Speed            EXIFTag = 0x0d
+	ImgDirectionRef  EXIFTag = 0x10
+	ImgDirection     EXIFTag = 0x11
+	MapDatum         EXIFTag = 0x12
+	DestLatitudeRef  EXIFTag = 0x13
+	DestLatitude     EXIFTag = 0x14
+	DestLongitudeRef EXIFTag = 0x15
+	DestLongitude    EXIFTag = 0x16
+	DestBearingRef   EXIFTag = 0x17
+	DestBearing      EXIFTag = 0x18
+	DestDistanceRef  EXIFTag = 0x19
+	DestDistance     EXIFTag = 0x1a
+	ProcessingMethod EXIFTag = 0x1b
+	Datestamp        EXIFTag = 0x1d
+	Differential     EXIFTag = 0x1e
 )
 
+type GPSExif struct {
+	Version              string    `json:"version"`
+	Altitude             float64   `json:"altitude"`
+	Latitude             float64   `json:"latitude"`
+	Longitude            float64   `json:"longitude"`
+	Timestamp            time.Time `json:"timestamp"`
+	Speed                string    `json:"speed"`
+	Direction            string    `json:"direction"`
+	MapDatum             string    `json:"mapDatum"`
+	DestinationLatitude  float64   `json:"destinationLatitude"`
+	DestinationLongitude float64   `json:"destinationLongitude"`
+	DestinationBearing   float64   `json:"destinationBearing"`
+	DestinationDistance  float64   `json:"destinationDistance"`
+	ProcessingMethod     string    `json:"processingMethod"`
+	Differential         string    `json:"differential"`
+}
+
+// TemporalData Temporal evidence with full precision
+type TemporalData struct {
+	DateCaptured        time.Time `json:"dateCaptured"`
+	CreateDate          time.Time `json:"createDate"`
+	ModifyDate          time.Time `json:"modifyDate"`
+	SubSecTime          string    `json:"subSecTime"`
+	SubSecTimeOriginal  string    `json:"subSecTimeOriginal"`
+	SubSecTimeDigitized string    `json:"subSecTimeDigitized"`
+	OffsetTime          string    `json:"offsetTime"`
+	OffsetTimeOriginal  string    `json:"offsetTimeOriginal"`
+	OffsetTimeDigitized string    `json:"offsetTimeDigitized"`
+}
+
+// DeviceData Device identification data
+type DeviceData struct {
+	Make             string `json:"make"`
+	Model            string `json:"model"`
+	BodySerialNumber string `json:"bodySerialNumber"`
+	SerialNumber     string `json:"serialNumber"`
+	CameraFirmware   string `json:"cameraFirmware"`
+	LensInfo         string `json:"lensInfo"`
+	LensMake         string `json:"lensMake"`
+	LensModel        string `json:"lensModel"`
+	LensSerialNumber string `json:"lensSerialNumber"`
+}
+
+// ImageProperties Image dimensions and properties
+type ImageProperties struct {
+	Width            int     `json:"width"`
+	Height           int     `json:"height"`
+	PixelXDimension  float64 `json:"pixelXDimension"`
+	PixelYDimension  float64 `json:"pixelYDimension"`
+	Orientation      string  `json:"orientation"`
+	ColorSpace       string  `json:"colorSpace"`
+	ComponentsConfig string  `json:"componentsConfiguration"`
+	FileSource       string  `json:"fileSource"`
+	SceneType        string  `json:"sceneType"`
+	ExifVersion      string  `json:"exifVersion"`
+	FlashpixVersion  string  `json:"flashpixVersion"`
+}
+
+// CameraSettings Camera settings used during capture
+type CameraSettings struct {
+	ExposureTime         string  `json:"exposureTime"`
+	FNumber              float64 `json:"fNumber"`
+	ExposureProgram      string  `json:"exposureProgram"`
+	ISO                  int     `json:"iso"`
+	FocalLength          float64 `json:"focalLength"`
+	MeteringMode         string  `json:"meteringMode"`
+	LightSource          string  `json:"lightSource"`
+	Flash                string  `json:"flash"`
+	FlashFired           string  `json:"flashFired"`
+	WhiteBalance         string  `json:"whiteBalance"`
+	SceneCaptureType     string  `json:"sceneCaptureType"`
+	SubjectDistanceRange string  `json:"subjectDistanceRange"`
+}
+
+// ProcessingData Post-processing and manipulation indicators
+type ProcessingData struct {
+	Software            string  `json:"software"`
+	ProcessingSoftware  string  `json:"processingSoftware"`
+	ImageEditor         string  `json:"imageEditor"`
+	DigitalZoomRatio    float64 `json:"digitalZoomRatio"`
+	Contrast            string  `json:"contrast"`
+	Saturation          string  `json:"saturation"`
+	Sharpness           string  `json:"sharpness"`
+	CompositeImage      string  `json:"compositeImage"`
+	CompositeImageCount int     `json:"compositeImageCount"`
+}
+
+// AuthorshipData Authorship and chain of custody
+type AuthorshipData struct {
+	Artist           string `json:"artist"`
+	Copyright        string `json:"copyright"`
+	ImageDescription string `json:"imageDescription"`
+	XPTitle          string `json:"xpTitle"`
+	XPComment        string `json:"xpComment"`
+	XPAuthor         string `json:"xpAuthor"`
+	XPKeywords       string `json:"xpKeywords"`
+	XPSubject        string `json:"xpSubject"`
+	UserComment      string `json:"userComment"`
+}
+
+// AuthenticityData Authenticity and integrity markers
+type AuthenticityData struct {
+	ImageUniqueID    string `json:"imageUniqueID"`
+	MakerNote        string `json:"makerNote"`
+	RelatedSoundFile string `json:"relatedSoundFile"`
+}
+
 type PhotoExifEvidence struct {
-	DateCaptured    time.Time `json:"dateCaptured"`
-	GPSLatitude     float64   `json:"gpsLatitude"`
-	GPSLongitude    float64   `json:"gpsLongitude"`
-	Make            string    `json:"make"`
-	Model           string    `json:"model"`
-	PixelXDimension float64   `json:"pixelXDimension"`
-	PixelYDimension float64   `json:"pixelYDimension"`
-	Software        string    `json:"software"`
-	Orientation     string    `json:"orientation"`
-	Flash           string    `json:"flash"`
+	Temporal     TemporalData     `json:"temporal"`
+	GPS          GPSExif          `json:"gps"`
+	Device       DeviceData       `json:"device"`
+	Image        ImageProperties  `json:"image"`
+	Camera       CameraSettings   `json:"camera"`
+	Processing   ProcessingData   `json:"processing"`
+	Authorship   AuthorshipData   `json:"authorship"`
+	Authenticity AuthenticityData `json:"authenticity"`
 }
 
 type IFDEntry struct {
@@ -271,13 +449,13 @@ func extractExifData(data []byte) (*PhotoExifEvidence, error) {
 
 		switch entry.Tag {
 		case Make:
-			metadata.Make = helper.GetString(entry, entryOffset)
+			metadata.Device.Make = helper.GetString(entry, entryOffset)
 		case Model:
-			metadata.Model = helper.GetString(entry, entryOffset)
+			metadata.Device.Model = helper.GetString(entry, entryOffset)
 		case Software:
-			metadata.Software = helper.GetString(entry, entryOffset)
+			metadata.Processing.Software = helper.GetString(entry, entryOffset)
 		case Orientation:
-			metadata.Orientation = parseOrientationValue(helper.GetUint16(entryOffset))
+			metadata.Image.Orientation = parseOrientationValue(helper.GetUint16(entryOffset))
 		case EXIFSubIFD:
 			exifSubIfdPointer := endian.Uint32(data[entryOffset+8 : entryOffset+12])
 			exifIfdOffset := tiffStart + int(exifSubIfdPointer)
@@ -312,13 +490,13 @@ func extractExifSubIFD(exifIfdOffset int, metadata *PhotoExifEvidence, helper *E
 				slog.Warn("Found capture timestamp, but it is an invalid format!", "captureDate", dateStr, "error", err)
 				continue
 			}
-			metadata.DateCaptured = captured
+			metadata.Temporal.DateCaptured = captured
 		case PixelXDimension:
-			metadata.PixelXDimension = float64(helper.GetUint16(entryOffset))
+			metadata.Image.PixelXDimension = float64(helper.GetUint16(entryOffset))
 		case PixelYDimension:
-			metadata.PixelYDimension = float64(helper.GetUint16(entryOffset))
+			metadata.Image.PixelYDimension = float64(helper.GetUint16(entryOffset))
 		case FlashFired:
-			metadata.Flash = parseFlashValue(helper.GetUint16(entryOffset))
+			metadata.Camera.Flash = parseFlashValue(helper.GetUint16(entryOffset))
 		}
 	}
 }
@@ -340,31 +518,31 @@ func extractGPSIFD(exifIfdOffset int, metadata *PhotoExifEvidence, helper *ExifV
 			"valueOffset", entry.ValueOffset)
 
 		switch entry.Tag {
-		case LatitudeDir:
+		case LatitudeRef:
 			latRef = helper.GetString(entry, entryOffset)
 		case Latitude:
-			metadata.GPSLatitude = helper.GetGPSCoord(entry)
+			metadata.GPS.Latitude = helper.GetGPSCoord(entry)
 			hasLat = true
-		case LongitudeDir:
+		case LongitudeRef:
 			longRef = helper.GetString(entry, entryOffset)
 		case Longitude:
-			metadata.GPSLongitude = helper.GetGPSCoord(entry)
+			metadata.GPS.Longitude = helper.GetGPSCoord(entry)
 			hasLong = true
 		}
 	}
 
 	if hasLat && latRef == "S" {
-		metadata.GPSLatitude *= -1
+		metadata.GPS.Latitude *= -1
 	}
-	if hasLat && (metadata.GPSLatitude < -90 || metadata.GPSLatitude > 90) {
-		slog.Warn("GPS latitude out of valid range", "lat", metadata.GPSLatitude)
+	if hasLat && (metadata.GPS.Latitude < -90 || metadata.GPS.Latitude > 90) {
+		slog.Warn("GPS latitude out of valid range", "lat", metadata.GPS.Latitude)
 	}
 
 	if hasLong && longRef == "W" {
-		metadata.GPSLongitude *= -1
+		metadata.GPS.Longitude *= -1
 	}
-	if hasLong && (metadata.GPSLongitude < -180 || metadata.GPSLongitude > 180) {
-		slog.Warn("GPS longitude out of valid range", "long", metadata.GPSLongitude)
+	if hasLong && (metadata.GPS.Longitude < -180 || metadata.GPS.Longitude > 180) {
+		slog.Warn("GPS longitude out of valid range", "long", metadata.GPS.Longitude)
 	}
 }
 
